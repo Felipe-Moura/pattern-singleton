9602b605106bc727d3a191f42a384302dcd1f5ef
Felipe Moura <flemc00@gmail.com>
2022-06-26 15:57:53 -0300

    first commit

---
+++ Singleton.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Singleton {
+	
+	private static Singleton firstInstance = null;
+	
+	private Singleton() {}
+	
+	public static Singleton getInstance() {
+		
+		if(firstInstance == null) {
+			firstInstance = new Singleton();
+		}
+		
+		return firstInstance;
+		
+	}
+
+}
